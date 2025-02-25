ab863f73157802b9031697375e8acfd925a075f6
Aditya Chopde <its.adityac@gmail.com>
2025-02-25 10:57:44 +0530

    added the email function in the handlers

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,64 +1,87 @@
 const Image = require("../models/image");
 const TextData = require("../models/textData");
 const User = require("../models/user");
+const { userNewTextDataStatus } = require("../sendemail");
 
-async function approveLoginHandler(res, res){
+async function approveLoginHandler(res, res) {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const approveLogin = await User.findByIdAndUpdate(id, { status: "Approved" });
-        return res.json({success: true, data: approveLogin})
+        return res.json({ success: true, data: approveLogin })
     } catch (err) {
-        return res.json({success: false, message: "Error Ocurreed", error: err.message})
+        return res.json({ success: false, message: "Error Ocurreed", error: err.message })
     }
 }
 
-async function rejectLoginHandler(res, res){
+async function rejectLoginHandler(res, res) {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const approveLogin = await User.findByIdAndUpdate(id, { status: "Rejected" });
-        return res.json({success: true, data: approveLogin})
+        return res.json({ success: true, data: approveLogin })
     } catch (err) {
-        return res.json({success: false, message: "Error Ocurreed", error: err.message})
+        return res.json({ success: false, message: "Error Ocurreed", error: err.message })
     }
 }
 
-async function approvePostHandler(req, res){
+async function approvePostHandler(req, res) {
     try {
-        const {id} = req.params;
-        const approveLogin = await Image.findByIdAndUpdate(id, { status: "Approved" });
-        return res.json({success: true, data: approveLogin})
+        const { id } = req.params;
+        const acceptImagePostRequest = await Image.findByIdAndUpdate(id, { status: "Accepted" });
+
+        // Send Email to the user
+        userNewImageEmailStatus(acceptImagePostRequest.name, acceptImagePostRequest.email, acceptImagePostRequest.department, acceptImagePostRequest.phone, acceptImagePostRequest.path, "Accepted")
+        // Send Email to the Admin
+        adminNewTextDataStatus(acceptImagePostRequest.name, acceptImagePostRequest.email, acceptImagePostRequest.department, acceptImagePostRequest.phone, acceptImagePostRequest.path, "Accepted")
+
+        return res.json({ success: true, data: acceptImagePostRequest })
     } catch (err) {
-        return res.json({success: false, message: "Error Ocurreed", error: err.message})
+        return res.json({ success: false, message: "Error Ocurreed", error: err.message })
     }
 }
 
-async function rejectPostHandler(req, res){
+async function rejectPostHandler(req, res) {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const rejectPost = await Image.findByIdAndUpdate(id, { status: "Rejected" });
-        return res.json({success: true, data: rejectPost})
+
+        // Send Email to the user
+        userNewImageEmailStatus(rejectPost.name, rejectPost.email, rejectPost.department, rejectPost.phone, rejectPost.path, "Rejected")
+        // Send Email to the Admin
+        adminNewTextDataStatus(rejectPost.name, rejectPost.email, rejectPost.department, rejectPost.phone, rejectPost.path, "Rejected")
+
+        return res.json({ success: true, data: rejectPost })
     } catch (err) {
-        return res.json({success: false, message: "Error Ocurreed", error: err.message})
+        return res.json({ success: false, message: "Error Ocurreed", error: err.message })
     }
 }
 
-async function acceptTextDataHandler(req, res){
+async function acceptTextDataHandler(req, res) {
     try {
-        const {id} = req.params;
-        const acceptTextData = await TextData.findByIdAndUpdate(id, { status: "Approved" });
-        return res.json({success: true, data: acceptTextData})
+        const { id } = req.params;
+        const acceptTextData = await TextData.findByIdAndUpdate(id, { status: "Accepted" });
+
+        // Sending email to the user
+        userNewTextDataStatus(acceptTextData.name, acceptTextData.email, acceptTextData.department, acceptTextData.phone, acceptTextData.title, acceptTextData.description, "Accepted")
+        // Sending email to admin
+        adminNewTextDataStatus(acceptTextData.name, acceptTextData.email, acceptTextData.department, acceptTextData.phone, acceptTextData.title, acceptTextData.description, "Accepted")
+        return res.json({ success: true, data: acceptTextData })
     } catch (err) {
-        return res.json({success: false, message: "Error Ocurreed", error: err.message})
+        return res.json({ success: false, message: "Error Ocurreed", error: err.message })
     }
 }
 
-async function rejectTextDataHandler(req, res){
+async function rejectTextDataHandler(req, res) {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const rejectTextData = await TextData.findByIdAndUpdate(id, { status: "Rejected" });
-        return res.json({success: true, data: rejectTextData})
+
+        // Sending email to the user
+        userNewTextDataStatus(acceptTextData.name, acceptTextData.email, acceptTextData.department, acceptTextData.phone, acceptTextData.title, acceptTextData.description, "Rejected")
+        // Sending email to admin
+        adminNewTextDataStatus(acceptTextData.name, acceptTextData.email, acceptTextData.department, acceptTextData.phone, acceptTextData.title, acceptTextData.description, "Rejected")
+        return res.json({ success: true, data: rejectTextData })
     } catch (err) {
-        return res.json({success: false, message: "Error Ocurreed", error: err.message})
+        return res.json({ success: false, message: "Error Ocurreed", error: err.message })
     }
 }
 
