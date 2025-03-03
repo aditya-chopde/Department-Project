82327759a825bc52ff9a802dbc4847b194840417
Aditya Chopde <its.adityac@gmail.com>
2025-03-03 12:52:58 +0530

    made some changes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,9 +2,9 @@ const express = require("express")
 const { rejectLoginHandler, approveLoginHandler, rejectPostHandler, approvePostHandler } = require("../controllers/admin")
 const router = express()
 
-router.post("/reject-login", rejectLoginHandler);
-router.post("/approve-login", approveLoginHandler);
-router.post("/reject-post", rejectPostHandler);
-router.post("/approve-post", approvePostHandler);
+router.post("/reject-login/:id", rejectLoginHandler);
+router.post("/approve-login/:id", approveLoginHandler);
+router.post("/reject-post/:id", rejectPostHandler);
+router.post("/approve-post/:id", approvePostHandler);
 
 module.exports = router;
